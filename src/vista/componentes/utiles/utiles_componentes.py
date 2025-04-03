5b2fe4eb9f3db167374a3d3c0531cc23d14b9104
Victor Nivelo <104579002+VictorNivelo@users.noreply.github.com>
2025-04-03 15:17:18 -0500

    ordenación del código

--- src/vista/componentes/utiles/utiles_componentes.py
+++ src/vista/componentes/utiles/utiles_componentes.py
@@ -1,28 +1,6 @@
 from vista.utiles.utiles_vista import establecer_icono_tema
 
 
-# Metodo para cerrar la ventana modal
-def cerrar_ventana_modal(ventana, componentes, controlador):
-    try:
-        # Eliminar referencias de los componentes en el controlador
-        for widget in componentes:
-            if widget in controlador.paneles:
-                controlador.paneles.remove(widget)
-            if widget in controlador.etiquetas:
-                controlador.etiquetas.remove(widget)
-            for nombre in list(controlador.botones.keys()):
-                if controlador.botones[nombre] == widget:
-                    del controlador.botones[nombre]
-        # Limpiar lista de componentes
-        componentes.clear()
-        # Liberar el modo modal
-        ventana.grab_release()
-        # Destruir la ventana
-        ventana.destroy()
-    except Exception as e:
-        print(f"Error durante la limpieza: {e}")
-
-
 # Metodo para configurar la ventana modal
 def configurar_ventana_modal(
     ventana_principal, ventana_modal, ancho, alto, titulo, color_fondo, funcion_cierre, controlador
@@ -44,3 +22,25 @@ def configurar_ventana_modal(
     ventana_modal.protocol("WM_DELETE_WINDOW", funcion_cierre)
     # Establecer el icono de la ventana modal
     establecer_icono_tema(ventana_modal, controlador.tema_interfaz)
+
+
+# Metodo para cerrar la ventana modal
+def cerrar_ventana_modal(ventana, componentes, controlador):
+    try:
+        # Eliminar referencias de los componentes en el controlador
+        for widget in componentes:
+            if widget in controlador.paneles:
+                controlador.paneles.remove(widget)
+            if widget in controlador.etiquetas:
+                controlador.etiquetas.remove(widget)
+            for nombre in list(controlador.botones.keys()):
+                if controlador.botones[nombre] == widget:
+                    del controlador.botones[nombre]
+        # Limpiar lista de componentes
+        componentes.clear()
+        # Liberar el modo modal
+        ventana.grab_release()
+        # Destruir la ventana
+        ventana.destroy()
+    except Exception as e:
+        print(f"Error durante la limpieza: {e}")
